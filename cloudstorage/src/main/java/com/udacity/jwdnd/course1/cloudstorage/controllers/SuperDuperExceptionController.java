4a3b0cffb71b20749e3de681f402886076264756
Marcos Cavalcante <marcoscavalcante@deloitte.co.uk>
2020-12-31 16:07:44 +0000

    fix: review comments

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SuperDuperExceptionController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SuperDuperExceptionController.java
@@ -2,15 +2,23 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import com.udacity.jwdnd.course1.cloudstorage.models.ResultStatusEnum;
 import com.udacity.jwdnd.course1.cloudstorage.services.ResultsService;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
 
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class SuperDuperExceptionController {
+
+    @Autowired
+    private Environment env;
 
     private ResultsService resultsService;
 
@@ -28,6 +36,15 @@ public class SuperDuperExceptionController {
     public String handleDuplicateFile(ConstraintViolationException exception, Model model) {
         this.resultsService.setAttributes(model, ResultStatusEnum.NOT_SAVED);
         model.addAttribute("error", exception.getMessage());
+        return "result";
+    }
+
+    @ExceptionHandler(value = { MultipartException.class, SizeLimitExceededException.class, FileSizeLimitExceededException.class})
+    public String handleFileSizeLimitExceptions(MultipartException exception, Model model) {
+        this.resultsService.setAttributes(model, ResultStatusEnum.NOT_SAVED);
+        String errorMsg = exception.getMessage().split(";")[0];
+        String maxFileSize = this.env.getProperty("spring.servlet.multipart.max-file-size");
+        model.addAttribute("error", errorMsg + ".\nMaximum file size is: " + maxFileSize + ".");
         return "result";
     }
 }
--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
@@ -2,9 +2,11 @@ package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
 import com.udacity.jwdnd.course1.cloudstorage.models.Note;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @Mapper
 public interface NoteMapper {
 
